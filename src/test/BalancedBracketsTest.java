5c35691d4fc8d2b7f77aa583e72129bb81c4f419
Pournima Rajagopal <pourni.r@gmail.com>
2021-05-23 17:37:20 -0500

    Initial commit

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,73 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+    // @Test
+    //public void emptyTest() {
+    //  assertEquals(true, true);
+    //}
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+      assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void onlyValidMultipleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][]"));
+    }
 
+    @Test
+    public void onlyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launchcode"));
+    }
+
+    @Test
+    public void validBracketsWithStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"));
+    }
+
+    @Test
+    public void validBracketsInMiddleOfStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launc[]hcode"));
+    }
+
+    @Test
+    public void validMultipleBracketsInMiddleOfStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launc[][][][][]hcode"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyInvalidOpenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void onlyInvalidClosedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void invalidOpenBracketsWithStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launchcode"));
+    }
+
+    @Test
+    public void invalidClosedBracketsWithStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launchcode"));
+    }
+
+    @Test
+    public void invalidOpenBracketsInMiddleOfStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launc[hcode"));
+    }
+
+    @Test
+    public void invalidClosedBracketsInMiddleOfStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launchc]ode"));
+    }
 }
